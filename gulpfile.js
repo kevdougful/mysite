c786967d4e54b13acc82c76cf4af04e4cc4115da
Kevin Coffey <kevdougful@gmail.com>
2016-03-24 15:40:05 -0500

    added gulp db tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var loopbackAngularSdk = require('gulp-loopback-sdk-angular');
+var exec = require('child_process').exec;
 
 // Generate AngularJS service for API
 gulp.task('api-service', function() {
@@ -12,4 +13,29 @@ gulp.task('api-service', function() {
         }))
         .pipe(rename('api-service.js'))
         .pipe(gulp.dest('./client/services'));
+});
+
+// Update DB Schema (do not drop tables)
+gulp.task('update-db', function(done) {
+    exec('node ./server/migrations/update.js', 
+    function(err, stdout, stderr) {
+        done(err);
+    });
+});
+
+// Rebuild DB Schema (drop tables, if necessary)
+gulp.task('rebuild-db', function(done) {
+    exec('node ./server/migrations/rebuild.js', 
+    function(err, stdout, stderr) {
+        done(err);
+    });
+});
+
+// Create LoopBack tables in DB (User, Role, etc.)
+// Note: This will drop tables if they already exist.
+gulp.task('create-lb-tables', function(done) {
+    exec('node ./server/migrations/create-lb-tables.js', 
+    function(err, stdout, stderr) {
+        done(err);
+    });
 });
