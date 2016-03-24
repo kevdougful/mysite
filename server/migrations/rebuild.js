ed02901792acce3199b07e455e721178fecebab8
Kevin Coffey <kevdougful@gmail.com>
2016-03-24 15:40:33 -0500

    handled datasource connection

--- server/migrations/rebuild.js
+++ server/migrations/rebuild.js
@@ -3,14 +3,24 @@
 var server = require('../server');
 var ds = server.dataSources.db;
 
-ds.isActual(function(err, inSync) {
-    if (!inSync) {
-        ds.automigrate(function(err, result) {
-            if (err) {
-                throw err;
-            } else {
-                ds.disconnect();
-            }
-        });
-    }
-});
+if (ds.connected) {
+    rebuild();
+} else {
+    ds.once('connected', function() {
+        rebuild();
+    });
+}
+
+function rebuild() {
+    ds.isActual(function(err, inSync) {
+        if (!inSync) {
+            ds.automigrate(function(err, result) {
+                if (err) {
+                    throw err;
+                } else {
+                    ds.disconnect();
+                }
+            });
+        }
+    });
+}
--- server/migrations/update.js
+++ server/migrations/update.js
@@ -3,14 +3,24 @@
 var server = require('../server');
 var ds = server.dataSources.db;
 
-ds.isActual(function(err, inSync) {
-    if (!inSync) {
-        ds.autoupdate(function(err, result) {
-            if (err) {
-                throw err;
-            } else {
-                ds.disconnect();
-            }
-        });
-    }
-});
+if (ds.connected) {
+    update();
+} else {
+    ds.once('connected', function() {
+        update();
+    });
+}
+
+function update() {
+    ds.isActual(function(err, inSync) {
+        if (!inSync) {
+            ds.autoupdate(function(err, result) {
+                if (err) {
+                    throw err;
+                } else {
+                    ds.disconnect();
+                }
+            });
+        }
+    });
+}
