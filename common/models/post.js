6eeb3bf17999b7c17db2dbf5d3fb1ee5037e11ed
Kevin Coffey <kevdougful@gmail.com>
2016-03-30 23:48:48 -0500

    added datePublished and changed author relation

--- common/models/post.js
+++ common/models/post.js
@@ -1,3 +1,18 @@
 module.exports = function(Post) {
-
+    Post.remoteMethod('publish', {
+        http: { path: '/:id/publish', verb: 'put' },
+        accepts: { arg: 'id', type: 'string', required: true, http: { source: 'path' }},
+        returns: { root: true, type: 'object' },
+        description: 'Marks a post as published.' 
+    });
+    
+    Post.publish = function(id, callback) {
+        Post.findById(id, function(err, post) {
+            post.updateAttributes({ published: true, datePublished: new Date() }, 
+                function(err, instance) {
+                    if (err) callback(err);
+                    if (!err) callback(null, instance);
+            });
+        });
+    }
 };
