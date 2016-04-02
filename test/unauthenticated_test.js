b515aee80b9bcfca2b6fe84ef875e10eb4b03d39
Kevin Coffey <kevdougful@gmail.com>
2016-04-02 12:32:20 -0500

    more tests

--- test/unauthenticated_test.js
+++ test/unauthenticated_test.js
@@ -20,6 +20,51 @@ describe('Unauthenticated User -', function() {
                 });
         });
         
+        it('should return array of Posts with author', function(done) {
+            request(app).get('/api/Posts?filter[include]=author')
+                .end(function(err, res) {
+                    assert.instanceOf(res.body, Array);
+                    res.body.should.include.something.that.has.property('author');
+                    done();
+                });
+        });
+        
+        it('should return array of Posts with category', function(done) {
+            request(app).get('/api/Posts?filter[include]=category')
+                .end(function(err, res) {
+                    assert.instanceOf(res.body, Array);
+                    res.body.should.include.something.that.has.property('category');
+                    done();
+                });
+        });
+        
+        it('should return array of Posts with tags', function(done) {
+            request(app).get('/api/Posts?filter[include]=tags')
+                .end(function(err, res) {
+                    assert.instanceOf(res.body, Array);
+                    res.body.should.include.something.that.has.property('tags');
+                    done();
+                });
+        });
+        
+        it('should return array of Posts with comments', function(done) {
+            request(app).get('/api/Posts?filter[include]=comments')
+                .end(function(err, res) {
+                    assert.instanceOf(res.body, Array);
+                    res.body.should.include.something.that.has.property('comments');
+                    done();
+                });
+        });
+        
+        it('should not allow unauthenticated user to create post', function(done) {
+            request(app).post('/api/Posts')
+                .end(function(err, res) {
+                    res.status.should.equal(401);
+                    done();
+                });
+        });
+            
+        
         it('should not return unpublished Posts', function(done) {
             request(app).get('/api/Posts?filter[where][published]=false')
                 .end(function(err, res) {
@@ -73,13 +118,100 @@ describe('Unauthenticated User -', function() {
     
     describe('Tags:', function() {
         
+        it('should return array of Tags', function(done) {
+            request(app).get('/api/Tags')
+                .expect(200)
+                .end(function(err, res) {
+                    assert.instanceOf(res.body, Array);
+                    done();
+                });
+        });
+        
+        it('should not allow unauthenticated user to create Tag', function(done) {
+            request(app).post('/api/Tags')
+                .end(function(err, res) {
+                    res.status.should.equal(401);
+                    done();
+                });
+        });
+        
+        it('should return array of Tags with category', function(done) {
+            request(app).get('/api/Tags?filter[include]=posts')
+                .end(function(err, res) {
+                    assert.instanceOf(res.body, Array);
+                    res.body.should.include.something.that.has.property('posts');
+                    done();
+                });
+        });
+            
     });
     
     describe('Categories:', function() {
         
+        it('should return array of Categories', function(done) {
+            request(app).get('/api/Categories')
+                .expect(200)
+                .end(function(err, res) {
+                    assert.instanceOf(res.body, Array);
+                    done();
+                });
+        });
+        
+        it('should not allow unauthenticated user to create Categories', function(done) {
+            request(app).post('/api/Categories')
+                .end(function(err, res) {
+                    res.status.should.equal(401);
+                    done();
+                });
+        });
+        
+        it('should return array of Categories with posts', function(done) {
+            request(app).get('/api/Categories?filter[include]=posts')
+                .end(function(err, res) {
+                    assert.instanceOf(res.body, Array);
+                    res.body.should.include.something.that.has.property('posts');
+                    done();
+                });
+        });
+        
     });
     
     describe('Comments:', function() {
+        
+        it('should return array of Comments', function(done) {
+            request(app).get('/api/Comments')
+                .expect(200)
+                .end(function(err, res) {
+                    assert.instanceOf(res.body, Array);
+                    done();
+                });
+        });
+        
+        it('should not allow unauthenticated user to create Comment', function(done) {
+            request(app).post('/api/Comments')
+                .end(function(err, res) {
+                    res.status.should.equal(401);
+                    done();
+                });
+        });
+        
+        it('should return array of Comments with post', function(done) {
+            request(app).get('/api/Comments?filter[include]=post')
+                .end(function(err, res) {
+                    assert.instanceOf(res.body, Array);
+                    res.body.should.include.something.that.has.property('post');
+                    done();
+                });
+        });
+        
+        it('should return array of Comments with commentor', function(done) {
+            request(app).get('/api/Comments?filter[include]=commentor')
+                .end(function(err, res) {
+                    assert.instanceOf(res.body, Array);
+                    res.body.should.include.something.that.has.property('commentor');
+                    done();
+                });
+        });
         
     });
 
